5044b920556d8b54109ea59437cd53c377fc5b3b
KianTigger <k.kordtomeikel@gmail.com>
2023-02-20 16:35:55 +0000

    Adding updated r2+1d weights, for 1 epoch of testing

--- efpredict/utils/EFPredict.py
+++ efpredict/utils/EFPredict.py
@@ -12,6 +12,10 @@ import tqdm
 
 import efpredict
 
+# import pretrained r2plus1d_18 model from torchvision
+from torchvision.models.video import r2plus1d_18, R2Plus1D_18_Weights
+
+
 
 @click.command("predict")
 @click.option("--data_dir", type=click.Path(exists=True, file_okay=False), default=None)
@@ -128,10 +132,18 @@ def setup_model(seed, model_name, pretrained, device, weights, frames, period, o
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
      # Set up model
-    model = torchvision.models.video.__dict__[
-        model_name](pretrained=pretrained)
-        # model_name](weights="R2Plus1D_18_Weights.DEFAULT") #TODO Should this be changed?
-
+     #TODO get rid of comments
+    # model = torchvision.models.video.__dict__[
+    #     model_name](pretrained=pretrained)
+    #     # model_name](weights="R2Plus1D_18_Weights.DEFAULT") #TODO Should this be changed?
+    #Setup model so if pretrained is true, then use the pretrained weights, otherwise use random weights
+    if pretrained:
+        #TODO make it so if other models are used, it chooses the correct weights
+        model = torchvision.models.video.__dict__[
+            model_name](weights=R2Plus1D_18_Weights.DEFAULT)
+    else:
+        model = torchvision.models.video.__dict__[
+            model_name](weights=None)
 
     # Replaced the last layer with a linear layer with 1 output
     model.fc = torch.nn.Linear(model.fc.in_features, 1)
