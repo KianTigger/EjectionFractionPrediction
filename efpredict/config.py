5bececc5e564e4c8c01b88954ebc08cda98651b5
KianTigger <k.kordtomeikel@gmail.com>
2023-04-11 13:08:19 +0100

    Fixing directory issues

--- efpredict/config.py
+++ efpredict/config.py
@@ -26,4 +26,6 @@ for filename in ["efpredict.cfg",
 CONFIG = types.SimpleNamespace(
     FILENAME=_FILENAME,
     DATA_DIR=_PARAM.get("data_dir", "a4c-video-dir/"),
-    UNLABELLED_DIR=_PARAM.get("unlabelled_dir", "a4c-video-dir/"))
+    UNLABELLED_DIR=_PARAM.get("unlabelled_dir", "a4c-video-dir/"),
+    PEDIACTRIC_DIR=_PARAM.get("pediatric_dir", "a4c-video-dir/"))
+
