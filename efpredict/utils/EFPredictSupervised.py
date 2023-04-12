c7b7e27178ec61431f4dc37fcf4b783b17c94bc8
KianTigger <k.kordtomeikel@gmail.com>
2023-04-12 14:22:12 +0100

    Making it so it doesn't repeat epochs

--- efpredict/utils/EFPredictSupervised.py
+++ efpredict/utils/EFPredictSupervised.py
@@ -93,9 +93,10 @@ def run_loops(output, device, model, optim, scheduler, num_epochs, batch_size, n
     with open(os.path.join(output, "log.csv"), "a") as f:
 
         model, optim, scheduler, epoch_resume, step_resume, bestLoss = helpFuncs.get_checkpoint(model, optim, scheduler, output, f)
-        if epoch_resume == 0:
-            epoch_resume = 1
-        print("Resuming from epoch #{}".format(epoch_resume), flush=True)
+        if epoch_resume != 0:
+            if step_resume == 0:
+                epoch_resume += 1
+            print("Resuming from epoch #{}".format(epoch_resume), flush=True)
         for epoch in range(epoch_resume, num_epochs + 1):
             print("Epoch #{}".format(epoch), flush=True)
             for phase in ['train', 'val']:
@@ -177,7 +178,7 @@ def run_epoch(model, dataloader, train, optim, device, step_resume, checkpoint_a
                     pbar.update(1)
                     continue
 
-                if step % (int(num_items//10)) == 0 and train:
+                if step != 0 and step % (int(num_items//10)) == 0 and train:
                     helpFuncs.save_checkpoint(model, checkpoint_args["period"], checkpoint_args["frames"], 
                             checkpoint_args["epoch"], step, checkpoint_args["output"], checkpoint_args["loss"], 
                             checkpoint_args["bestLoss"], optim, checkpoint_args["scheduler"])
