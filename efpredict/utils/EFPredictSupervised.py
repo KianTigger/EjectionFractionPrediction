a1d8835ea2abc9cdd069517bbf7d827cfaf7d4f1
KianTigger <k.kordtomeikel@gmail.com>
2023-04-13 16:09:36 +0100

    Fixing datasets issue

--- efpredict/utils/EFPredictSupervised.py
+++ efpredict/utils/EFPredictSupervised.py
@@ -192,7 +192,7 @@ def run_epoch(model, dataloader, train, optim, device, step_resume, checkpoint_a
                 print("outcome shape0:", outcome.shape)
                     
                 X = torch.stack(X_batch).to(device)
-                outcome_lengths = X_batch[0].shape[0]
+                outcome_lengths = X_batch[0].shape[0]/outcome.shape[0]
                 duplicated_outcome = np.repeat(outcome, outcome_lengths)
                 y.append(duplicated_outcome)
 
