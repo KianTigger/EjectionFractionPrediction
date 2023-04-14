195a7ce4b03897beff29071ba556e7e9b0c2b01f
KianTigger <33599443+KianTigger@users.noreply.github.com>
2023-04-14 15:18:27 +0100

    ensuring semi-supervised working

--- efpredict/utils/EFPredict.py
+++ efpredict/utils/EFPredict.py
@@ -265,13 +265,14 @@ def run_epoch(model, labelled_dataloader, train, optim, device, step_resume, che
                     
                     # Sample a batch from the unlabelled dataset
                     try:
-                        unlabelled_X, _ = next(unlabelled_iterator)
+                        unlabelled_X, temp = next(unlabelled_iterator)
                     except StopIteration:
                         unlabelled_iterator = iter(unlabelled_dataloader)
-                        unlabelled_X, _ = next(unlabelled_iterator)
+                        unlabelled_X, temp = next(unlabelled_iterator)
                     
                     #print information about unlabelled_X
                     print("unlabelled_X: ", unlabelled_X)
+                    print("temp: ", temp)
                     print("type(unlabelled_X): ", type(unlabelled_X))
                     print("len(unlabelled_X): ", len(unlabelled_X))
                     print("type(unlabelled_X[0]): ", type(unlabelled_X[0]))
