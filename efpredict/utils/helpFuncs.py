2ffd2bb476f472e212596d6156743ae95bc64bc5
KianTigger <33599443+KianTigger@users.noreply.github.com>
2023-04-15 00:58:30 +0100

    Fxing tensor size issue

--- efpredict/utils/helpFuncs.py
+++ efpredict/utils/helpFuncs.py
@@ -325,46 +325,39 @@ def get_labelled_loss(loss_type, outputs, outcome):
     return loss
 
 def get_unlabelled_loss(loss_type, model, unlabelled_X, outputs, alpha=0.1):
+    
+    size_diff = outputs.size(0) - unlabelled_outputs.size(0)    
+    # Pad the smaller tensor with zeros
+    if size_diff > 0:
+        padding = torch.zeros(size_diff, *unlabelled_outputs.size()[1:], device=unlabelled_outputs.device)
+        unlabelled_outputs = torch.cat((unlabelled_outputs, padding), dim=0)
+    elif size_diff < 0:
+        padding = torch.zeros(-size_diff, *outputs.size()[1:], device=outputs.device)
+        outputs = torch.cat((outputs, padding), dim=0)
+    
     if loss_type == "PSEUDO":
         # Generate pseudo-labels for the unlabelled data
         with torch.no_grad():
             pseudo_labels = model(unlabelled_X).detach()
-
         # Compute loss using the pseudo-labels
         unlabelled_loss = torch.nn.functional.mse_loss(model(unlabelled_X).view(-1), pseudo_labels.view(-1))
-    
     elif loss_type == "MSE":
         # Compute mse loss between labelled and unlabelled data
         unlabelled_outputs = model(unlabelled_X)
-        size_diff = outputs.size(0) - unlabelled_outputs.size(0)
-
-        # Pad the smaller tensor with zeros
-        if size_diff > 0:
-            padding = torch.zeros(size_diff, *unlabelled_outputs.size()[1:], device=unlabelled_outputs.device)
-            unlabelled_outputs = torch.cat((unlabelled_outputs, padding), dim=0)
-        elif size_diff < 0:
-            padding = torch.zeros(-size_diff, *outputs.size()[1:], device=outputs.device)
-            outputs = torch.cat((outputs, padding), dim=0)
-
         unlabelled_loss = torch.nn.functional.mse_loss(outputs.view(-1), unlabelled_outputs.view(-1))
-
         unlabelled_loss *= alpha
-
     elif loss_type == "MAE":
         # Compute mae loss between labelled and unlabelled data
         unlabelled_outputs = model(unlabelled_X)
         unlabelled_loss = torch.nn.functional.l1_loss(outputs.view(-1), unlabelled_outputs.view(-1))
-
     elif loss_type == "HUBER":
         # Compute huber loss between labelled and unlabelled data
         unlabelled_outputs = model(unlabelled_X)
         unlabelled_loss = torch.nn.functional.smooth_l1_loss(outputs.view(-1), unlabelled_outputs.view(-1))
-
     elif loss_type == "LOGCOSH":
         # Compute log-cosh loss between labelled and unlabelled data
         unlabelled_outputs = model(unlabelled_X)
         unlabelled_loss = log_cosh_loss(outputs.view(-1), unlabelled_outputs.view(-1))
-
     else:
         raise ValueError("Invalid loss_type specified")
 
